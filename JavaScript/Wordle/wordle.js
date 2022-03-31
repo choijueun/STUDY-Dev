e4a70def9a82a9f345d31943c5b5f829c6f66700
choijueun <songjueun815@gmail.com>
2022-03-31 16:19:47 +0900

    한글 정규식

--- JavaScript/Wordle/wordle.js
+++ JavaScript/Wordle/wordle.js
@@ -1,8 +1,20 @@
 $(function(){
+    // 한글 정규식 검사
+    function kRe(str) {
+        const re = /[ㄱ-ㅎㅏ-ㅣ가-힣]/g; 
+        if (re.test(str)) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
     var answer = 'abcde';
     var user_inputs = $('input')
-    $('button').click(function(){
-        console.log(user_inputs)
+    $('.button').click(function(){
         console.log(user_inputs[0].value)
+        console.log(kRe(user_inputs[0].value))
     });
+
+    
 });
